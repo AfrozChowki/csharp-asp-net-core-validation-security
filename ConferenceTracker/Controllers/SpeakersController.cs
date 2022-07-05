9baa5c5c49b9c40523830c19bb55efbcdab00eac
Afroz Chowki <Afroz.Chowki@wolterskluwer.com>
2022-07-05 14:14:00 +0530

    - Updated code as per "Improve Security Using Validation" course

--- ConferenceTracker/Controllers/SpeakersController.cs
+++ ConferenceTracker/Controllers/SpeakersController.cs
@@ -6,126 +6,132 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ConferenceTracker.Controllers
 {
-    public class SpeakersController : Controller
-    {
-        private readonly ISpeakerRepository _speakerRepository;
+	public class SpeakersController : Controller
+	{
+		private readonly ISpeakerRepository _speakerRepository;
 
-        public SpeakersController(ISpeakerRepository speakerRepository)
-        {
-            _speakerRepository = speakerRepository;
-        }
+		public SpeakersController(ISpeakerRepository speakerRepository)
+		{
+			_speakerRepository = speakerRepository;
+		}
 
-        public IActionResult Index()
-        {
-            var speakers = _speakerRepository.GetAllSpeakers();
-            return View(speakers);
-        }
+		public IActionResult Index()
+		{
+			var speakers = _speakerRepository.GetAllSpeakers();
+			return View(speakers);
+		}
 
-        public IActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		public IActionResult Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var speaker = _speakerRepository.GetSpeaker((int)id);
-            if (speaker == null)
-            {
-                return NotFound();
-            }
+			var speaker = _speakerRepository.GetSpeaker((int)id);
+			if (speaker == null)
+			{
+				return NotFound();
+			}
 
-            return View(speaker);
-        }
+			return View(speaker);
+		}
 
-        [HttpGet]
-        [Authorize(Roles = "Administrators")]
-        public IActionResult Create()
-        {
-            return View();
-        }
+		[HttpGet]
+		[Authorize(Roles = "Administrators")]
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        [Authorize(Roles = "Administrators")]
-        public IActionResult Create(Speaker speaker)
-        {
-            _speakerRepository.Create(speaker);
-            return RedirectToAction(nameof(Index));
-        }
+		[HttpPost, Authorize(Roles = "Administrators"), ValidateAntiForgeryToken]
+		public IActionResult Create([Bind("Id,FirstName,LastName,Description,EmailAddress,PhoneNumber")] Speaker speaker)
+		{
+			if (ModelState.IsValid)
+			{
+				_speakerRepository.Create(speaker);
+				return RedirectToAction(nameof(Index));
+			}
+			else
+			{
+				return View(speaker);
+			}
+		}
 
-        [HttpGet]
-        [Authorize(Roles = "Administrators")]
-        public IActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		[HttpGet]
+		[Authorize(Roles = "Administrators")]
+		public IActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var speaker = _speakerRepository.GetSpeaker((int)id);
-            if (speaker == null)
-            {
-                return NotFound();
-            }
-            return View(speaker);
-        }
+			var speaker = _speakerRepository.GetSpeaker((int)id);
+			if (speaker == null)
+			{
+				return NotFound();
+			}
+			return View(speaker);
+		}
 
-        [HttpPost]
-        [Authorize(Roles = "Administrators")]
-        public IActionResult Edit(int id, Speaker speaker)
-        {
-            if (id != speaker?.Id)
-            {
-                return NotFound();
-            }
+		[HttpPost]
+		[Authorize(Roles = "Administrators")]
+		public IActionResult Edit(int id, Speaker speaker)
+		{
+			if (id != speaker?.Id)
+			{
+				return NotFound();
+			}
 
-            try
-            {
-                _speakerRepository.Update(speaker);
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SpeakerExists(speaker.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
+			try
+			{
+				_speakerRepository.Update(speaker);
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!SpeakerExists(speaker.Id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+			return RedirectToAction(nameof(Index));
+		}
 
-        [HttpGet]
-        [Authorize(Roles = "Administrators")]
-        public IActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		[HttpGet]
+		[Authorize(Roles = "Administrators")]
+		public IActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var speaker = _speakerRepository.GetSpeaker((int)id);
-            if (speaker == null)
-            {
-                return NotFound();
-            }
+			var speaker = _speakerRepository.GetSpeaker((int)id);
+			if (speaker == null)
+			{
+				return NotFound();
+			}
 
-            return View(speaker);
-        }
+			return View(speaker);
+		}
 
-        [HttpPost, ActionName("Delete")]
-        [Authorize(Roles = "Administrators")]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            var speaker = _speakerRepository.GetSpeaker(id);
-            _speakerRepository.Delete(speaker);
-            return RedirectToAction(nameof(Index));
-        }
+		[HttpPost, ActionName("Delete")]
+		[Authorize(Roles = "Administrators")]
+		public IActionResult DeleteConfirmed(int id)
+		{
+			var speaker = _speakerRepository.GetSpeaker(id);
+			_speakerRepository.Delete(speaker);
+			return RedirectToAction(nameof(Index));
+		}
 
-        private bool SpeakerExists(int id)
-        {
-            return (_speakerRepository.GetSpeaker(id) != null);
-        }
-    }
+		private bool SpeakerExists(int id)
+		{
+			return (_speakerRepository.GetSpeaker(id) != null);
+		}
+	}
 }
--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -1,21 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
-    {
-        public int Id { get; set; }
-        [Display(Name = "First name")]
-        public string FirstName { get; set; }
-        [Display(Name = "Last name")]
-        public string LastName { get; set; }
-        public string Description { get; set; }
-        [Display(Name = "Email Address")]
-        public string EmailAddress { get; set; }
-        [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; }
-        public bool IsStaff { get; set; }
-    }
+	public class Speaker : IValidatableObject
+	{
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		[Display(Name = "First name")]
+		[DataType(DataType.Text)]
+		[StringLength(100, MinimumLength = 2, ErrorMessage = "First name should be at least 2 characters long, but no longer than 100 characters.")]
+		public string FirstName { get; set; }
+
+		[Required]
+		[Display(Name = "Last name")]
+		[DataType(DataType.Text)]
+		[StringLength(100, MinimumLength = 2, ErrorMessage = "Last name should be at least 2 characters long, but no longer than 100 characters.")]
+		public string LastName { get; set; }
+
+		[Required]
+		[DataType(DataType.MultilineText)]
+		[StringLength(500, MinimumLength = 10, ErrorMessage = "Name should be at least 10 characters long, but no longer than 500 characters.")]
+		public string Description { get; set; }
+
+		[DataType(DataType.EmailAddress)]
+		[Display(Name = "Email Address")]
+		public string EmailAddress { get; set; }
+
+		[DataType(DataType.PhoneNumber)]
+		[Display(Name = "Phone Number")]
+		public string PhoneNumber { get; set; }
+
+		public bool IsStaff { get; set; }
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			List<ValidationResult> validationResults = new List<ValidationResult>();
+			if (!string.IsNullOrEmpty(this.EmailAddress) && this.EmailAddress.Contains("TechnologyLiveConference.com",
+					 StringComparison.InvariantCulture))
+			{
+				validationResults.Add(new ValidationResult("Technology Live Conference staff should not use their conference email addresses."));
+			}
+
+			return validationResults;
+		}
+	}
 }
